718a206ac36b0f37cef61ddb66c58cadf42d0117
Edwin López <ledwin770@gmail.com>
2022-07-09 00:24:31 -0600

    Agregando procedimiento para colaboradores

--- Proyecto/DISEÑO DEL BD Servicio de Manteniemiento/Login.sql
+++ Proyecto/DISEÑO DEL BD Servicio de Manteniemiento/Login.sql
@@ -5,6 +5,12 @@
  -- Creando Login
 create login AdminMantenimiento with password = 'proyecto123'
 
+-- Para eliminar login
+/*SELECT name, suser_sname(owner_sid) AS DBOwner FROM sys.databases
+use Servicios_de_mantenimiento
+sp_changedbowner 'sa'
+drop login AdminMantenimiento*/
+
 go
 
 --Agregando Rol de Servidor (Administrador)
