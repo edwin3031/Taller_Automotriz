5cb1a9513c500aa034148795673c51d3ff437902
Edwin López <ledwin770@gmail.com>
2023-02-20 08:37:37 -0600

    Agregando login vendedor bodeguero

--- Proyecto/DISEÑO DEL BD Servicio de Manteniemiento/Login.sql
+++ Proyecto/DISEÑO DEL BD Servicio de Manteniemiento/Login.sql
@@ -26,6 +26,59 @@ sp_adduser AdminMantenimiento, AdminMantenimiento
 --Select @@SERVERNAME
 
 go
+create login Vendedor with password ='vende123'
+
+create user Derian34 from login Vendedor
+
+grant select, update, delete on Cliente to Derian34
+grant select, update, delete on Repuesto to Derian34
+grant select, update, delete on Proveedor to Derian34
+grant select, update, delete on Vehículo to Derian34
+
+grant execute on database :: Servicios_de_mantenimiento to Derian34
+grant create procedure on database :: Servicios_de_mantenimiento to Derian34
+grant select on Repuesto to Derian34
+grant select on Proveedor to Derian34
+grant select on Vehículo to Derian34
+
+
+go
+create login Jefe_Mecanico with password ='meca123'
+
+create user JaviRo from login Jefe_Mecanico
+
+
+grant select, update, delete on Repuesto to JaviRo
+grant select, update, delete on Detalle_Repuesto to JaviRo
+grant select, update, delete on Proveedor to JaviRo
+grant select, update, delete on Mantenimiento to JaviRo
+grant select, update, delete on Detalle_Mantenimiento to JaviRo
+grant select, update, delete on Mecánico to JaviRo
+
+grant execute on database :: Servicios_de_mantenimiento to JaviRo
+grant create procedure on database :: Servicios_de_mantenimiento to JaviRo
+grant select on Repuesto to Derian34
+grant select on Proveedor to Derian34
+grant select on Vehículo to Derian34
+
+
+create login Bodeguero with password ='bode123'
+
+create user Luisao from login Bodeguero
+
+
+grant select, update, delete on Repuesto to Luisao
+grant select, update, delete on Detalle_Repuesto to Luisao
+grant select, update, delete on Proveedor to Luisao
+
+
+grant execute on database :: Servicios_de_mantenimiento to Luisao
+grant create procedure on database :: Servicios_de_mantenimiento to Luisao
+
+go
+
+sp_adduser Vendedor, Vendedor
+
 
 
 	/*Creedenciales para login:
@@ -33,7 +86,7 @@ go
 	Contraseña: 2022*/
 
 	--Creando Roles
-	create role Vendedor
+	/*create role Vendedor
 
 	grant select on database :: Servicios_de_mantenimiento to Vendedor
 
@@ -46,4 +99,6 @@ go
 	alter schema dbo transfer Empleados.Colaborador
 	alter schema dbo transfer Empleados.Usuario
 
-	exec Mostrar_Usuarios
+	exec Mostrar_Usuarios*/
+
+	SELECT * FROM Usuario
